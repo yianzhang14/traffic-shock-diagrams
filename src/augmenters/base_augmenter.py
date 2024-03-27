2eb4364be3a4d7fdbf34eb8e6a2c3b20b4686ae2
yianzhang14 <78504318+yianzhang14@users.noreply.github.com>
2024-03-27 00:42:02 -0500

    added documentation to augmenters

--- src/augmenters/base_augmenter.py
+++ src/augmenters/base_augmenter.py
@@ -1,6 +1,22 @@
-from abc import ABC
+from abc import ABC, abstractmethod
+
+from sortedcontainers import SortedList
+
+from src.diagram_utils import Interface
 
 
 # TODO: fully flesh this out
 class TrafficAugmenter(ABC):
-    pass
+    """Abstract base class of all augmenters of the dt-space."""
+
+    @abstractmethod
+    def init(self, simulation_time: float, events: SortedList, interfaces: list[Interface]):
+        """Initializes an augmenter in the shockwave drawer life cycle. Creates all events the
+        augment would generate during the simulation time of the drawer.
+
+        Args:
+            simulation_time (float): how long the simulation will last for
+            events (SortedList): reference to the event queue
+            interfaces (list[Interface]): reference to the list of interfaces
+        """
+        pass
--- src/augmenters/traffic_light.py
+++ src/augmenters/traffic_light.py
@@ -1,11 +1,15 @@
 from sortedcontainers import SortedList
 
 from ..diagram_utils import CapacityEvent, Interface, dtPoint
-from ..fundamental_diagram import FundamentalDiagram
 from .base_augmenter import TrafficAugmenter
 
 
 class TrafficLight(TrafficAugmenter):
+    """Specialization of augmenter for traffic lights. Traffic light augmenters cause capacity to
+    drop to 0 for a specific period of time, followed by the release of that limitation.
+    This occurs is a configurable cyclical manner.
+    """
+
     id: int = 0
 
     def __init__(
@@ -15,22 +19,39 @@ class TrafficLight(TrafficAugmenter):
         blocking_states: tuple[bool, ...],
         init_state: int = 0,
     ):
+        """Traffic light constructor.
+
+        Args:
+            pos (float): the position at which the traffic light is located (meters)
+            cycles (tuple[float, ...]): the times of each traffic light cycle
+            blocking_states (tuple[bool, ...]): whether or not each cycle is blocking or not
+            init_state (int, optional): initial state of the traffic light. Defaults to 0.
+
+        Raises:
+            ValueError: the state must be a valid index of cycle
+            ValueError: each cycle of the traffic light must be defined to be blocking or not
+        """
         self.pos = pos  # where the traffic light is
         self.cycles = cycles  # list of the delays of each of the cycles
-        self.state = init_state  # initial state -- an index for self.cycles
+        self.state = init_state  # initial state -- an index for self.cycles (will wrap around)
         self.blocking_states = (
             blocking_states  # list of whether each corresponding cycle is blocking or not
         )
 
-        assert self.state < len(self.cycles) and self.state >= 0
-        assert len(blocking_states) == len(cycles)
+        if not (self.state < len(self.cycles) and self.state >= 0):
+            raise ValueError("The provided initial state is invalid.")
 
+        if len(blocking_states) != len(cycles):
+            raise ValueError("The lengths of the blocking states and cycles don't match")
+
+        # generate an unique id
         self.id = TrafficLight.id
         TrafficLight.id += 1
 
     def init(self, simulation_time: float, events: SortedList, interfaces: list[Interface]):
         time = 0
 
+        # continue adding capacity events to the event queue until we are out of time
         while time <= simulation_time:
             if self.blocking_states[self.state]:
                 start = dtPoint(time, self.pos)
