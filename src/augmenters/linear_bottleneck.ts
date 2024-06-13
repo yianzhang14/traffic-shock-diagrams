7673b80e089c2495ab5cc1fe425f760c38095366
yianzhang14 <78504318+yianzhang14@users.noreply.github.com>
2024-06-12 22:07:52 -0400

    fix linting

--- src/augmenters/linear_bottleneck.ts
+++ src/augmenters/linear_bottleneck.ts
@@ -1,5 +1,5 @@
 import { CapacityEvent, UserInterface, dtPoint, float_isclose } from "../drawer_utils";
-import { ShockwaveDrawer}  from "../shockwave_drawer";
+import { ShockwaveDrawer }  from "../shockwave_drawer";
 import { CapacityBottleneck } from "./base_augmenter";
 
 
