7c4fa396b1e54514435c3f570783ecbb03f8da5f
Ian Zhang <yianzhang14@gmail.com>
2024-09-05 12:07:16 -0500

    saving things

--- main.ts
+++ main.ts
@@ -3,13 +3,13 @@ import * as fs from "fs";
 import { HorizontalBottleneck, CapacityBottleneck, FundamentalDiagram, ShockwaveDrawer } from "./src/index";
 
 // alternatively, look at getShockwave
-const diagram = new FundamentalDiagram(2, 5.0, 1.0, 1.0);
+const diagram = new FundamentalDiagram(3, 5.0, 1.0, 1.0);
 
 const augments: CapacityBottleneck[] = [];
-augments.push(new HorizontalBottleneck(40, 10, 20, 1));
-augments.push(new HorizontalBottleneck(30, 25, 40, 0));
+augments.push(new HorizontalBottleneck(30, 10, 20, 1.5));
+// augments.push(new HorizontalBottleneck(30, 25, 40, 0));
 
 const drawer = new ShockwaveDrawer(diagram, augments);
 
-drawer.run(20);
+drawer.run(30);
 fs.writeFile("output.json", JSON.stringify(drawer.generateFigure(100, true, false)), (err) => {console.error(err);});
